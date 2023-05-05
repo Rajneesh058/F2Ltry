6723d8a0e66f56289a1757823cb8bd43c2b7bc7a
MOVIESWORLDBoy <117733454+MOVIESWORLDBoy@users.noreply.github.com>
2023-05-05 14:57:48 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL'))
-    FORCE_SUB = str(getenv('FORCE_SUB', None))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
